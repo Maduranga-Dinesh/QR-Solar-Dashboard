e558242930783d5c2f29109ce0924a7aff4158d9
Rangana99 <mdkrangana@gmail.com>
2024-04-22 23:15:01 +0530

    Home page and routes

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
-Route::get('/about', [UserController::class, 'about'])->name('about');
+// Route::get('/about', [UserController::class, 'about'])->name('about');
 
 Route::controller(AuthController::class)->group(function () {
     Route::get('register', 'register')->name('register');
@@ -36,20 +36,20 @@ Route::controller(AuthController::class)->group(function () {
 //Normal Users Routes List
 Route::middleware(['auth', 'user-access:user'])->group(function () {
     Route::get('/home', [HomeController::class, 'index'])->name('home');
-    Route::get('/profile', [UserController::class, 'userprofile'])->name('profile');
+    // Route::get('/profile', [UserController::class, 'userprofile'])->name('profile');
 });
 
 //Admin Routes List
-Route::middleware(['auth', 'user-access:admin'])->group(function () {
-    Route::get('/admin/home', [HomeController::class, 'adminHome'])->name('admin/home');
+// Route::middleware(['auth', 'user-access:admin'])->group(function () {
+//     Route::get('/admin/home', [HomeController::class, 'adminHome'])->name('admin/home');
 
-    Route::get('/admin/profile', [AdminController::class, 'profilepage'])->name('admin/profile');
+//     Route::get('/admin/profile', [AdminController::class, 'profilepage'])->name('admin/profile');
 
-    Route::get('/admin/products', [ProductController::class, 'index'])->name('admin/products');
-    Route::get('/admin/products/create', [ProductController::class, 'create'])->name('admin/products/create');
-    Route::post('/admin/products/store', [ProductController::class, 'store'])->name('admin/products/store');
-    Route::get('/admin/products/show/{id}', [ProductController::class, 'show'])->name('admin/products/show');
-    Route::get('/admin/products/edit/{id}', [ProductController::class, 'edit'])->name('admin/products/edit');
-    Route::put('/admin/products/edit/{id}', [ProductController::class, 'update'])->name('admin/products/update');
-    Route::delete('/admin/products/destroy/{id}', [ProductController::class, 'destroy'])->name('admin/products/destroy');
-});
+//     Route::get('/admin/products', [ProductController::class, 'index'])->name('admin/products');
+//     Route::get('/admin/products/create', [ProductController::class, 'create'])->name('admin/products/create');
+//     Route::post('/admin/products/store', [ProductController::class, 'store'])->name('admin/products/store');
+//     Route::get('/admin/products/show/{id}', [ProductController::class, 'show'])->name('admin/products/show');
+//     Route::get('/admin/products/edit/{id}', [ProductController::class, 'edit'])->name('admin/products/edit');
+//     Route::put('/admin/products/edit/{id}', [ProductController::class, 'update'])->name('admin/products/update');
+//     Route::delete('/admin/products/destroy/{id}', [ProductController::class, 'destroy'])->name('admin/products/destroy');
+// });
