4f572139f2ee09275b8a5a06b4818ae44f431806
Rangana99 <mdkrangana@gmail.com>
2024-04-22 22:30:12 +0530

    Log in interface and routes

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class User extends Authenticatable
 {
@@ -37,11 +38,14 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
-    protected function casts(): array
-    {
-        return [
+    protected $casts =[
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
-    }
+        protected function type(): Attribute
+        {
+            return new Attribute(
+                get: fn ($value) =>  ["user", "admin"][$value],
+            );
+        }
 }
