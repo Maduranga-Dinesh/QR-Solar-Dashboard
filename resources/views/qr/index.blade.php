52144c09d61c951ead1e9314fca4523561aa43ca
Rangana99 <mdkrangana@gmail.com>
2024-04-28 08:52:00 +0530

    add 20 px

--- resources/views/qr/index.blade.php
+++ resources/views/qr/index.blade.php
@@ -5,15 +5,20 @@
 @section('contents')
 <div>
     <h1 class="font-bold text-2xl ml-3">QR ID List</h1>
-    <a href="{{ route('admin/qr/create') }}" class="text-white float-right bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add QR ID</a>
     <hr />
+    <a href="{{ route('admin/qr/create') }}" class="text-white float-right bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add QR ID</a>
+
 
     @if(Session::has('success'))
     <div class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
         {{ Session::get('success') }}
     </div>
     @endif
-
+<style>
+     hr {
+            margin-bottom: 20px; /* Add 20px margin below the <hr> */
+        }
+</style>
     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr class="w-36">
